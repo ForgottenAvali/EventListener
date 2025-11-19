0f32081e14892a493d46fe92cc888df04f9185f8
Noodle <det3ctiver.business@gmail.com>
2025-11-19 01:51:02 -0600

    Update groups.py

--- EventListener/EventListener/data/website/groups.py
+++ EventListener/EventListener/data/website/groups.py
@@ -1,25 +1,24 @@
 import os, json, requests, asyncio
-from dotenv import load_dotenv
 
 
 from data.extra import ts
+import data.env_config as config
 
 
-load_dotenv()
-CONTACT = os.getenv("CONTACT")
-ENDPOINT_BASE_GROUP = os.getenv("ENDPOINT_BASE_GROUP")
-API_KEY = os.getenv("API_KEY")
+config.CONTACT
+config.ENDPOINT_BASE_GROUP
+config.API_KEY
 
 
 async def add_group_to_api(vrc_group_id: str, name: str):
-    if not ENDPOINT_BASE_GROUP or not API_KEY:
+    if not config.ENDPOINT_BASE_GROUP or not config.API_KEY:
         print(f"{ts()} [Website] Skipping group creation (no endpoint/API key).")
         return
 
     headers = {
         "content-type": "application/json",
-        "x-api-key": API_KEY,
-        "user-agent": str(CONTACT),
+        "x-api-key": config.API_KEY,
+        "user-agent": str(config.CONTACT),
     }
 
     payload = {
@@ -30,7 +29,7 @@ async def add_group_to_api(vrc_group_id: str, name: str):
     print(f"{ts()} [Website] Creating group '{name}' ({vrc_group_id})...")
 
     try:
-        response = requests.post(ENDPOINT_BASE_GROUP, headers=headers, json=payload, timeout=90)
+        response = requests.post(config.ENDPOINT_BASE_GROUP, headers=headers, json=payload, timeout=90)
 
         try:
             response_text = json.dumps(response.json(), indent=2)
@@ -47,16 +46,16 @@ async def add_group_to_api(vrc_group_id: str, name: str):
 
 
 async def update_group_on_api(vrc_group_id: str, name: str):
-    if not ENDPOINT_BASE_GROUP or not API_KEY:
+    if not config.ENDPOINT_BASE_GROUP or not config.API_KEY:
         print(f"{ts()} [Website] Skipping group update (no endpoint/API key).")
         return
 
-    endpoint = f"{ENDPOINT_BASE_GROUP}/{vrc_group_id}"
+    endpoint = f"{config.ENDPOINT_BASE_GROUP}/{vrc_group_id}"
 
     headers = {
         "content-type": "application/json",
-        "x-api-key": API_KEY,
-        "user-agent": str(CONTACT),
+        "x-api-key": config.API_KEY,
+        "user-agent": str(config.CONTACT),
     }
 
     payload = {
@@ -89,15 +88,15 @@ async def update_group_on_api(vrc_group_id: str, name: str):
 
 
 async def delete_group_on_api(vrc_group_id: str):
-    if not ENDPOINT_BASE_GROUP or not API_KEY:
+    if not config.ENDPOINT_BASE_GROUP or not config.API_KEY:
         print(f"{ts()} [Website] Skipping group deletion (no endpoint/API key).")
         return
 
-    endpoint = f"{ENDPOINT_BASE_GROUP}/{vrc_group_id}"
+    endpoint = f"{config.ENDPOINT_BASE_GROUP}/{vrc_group_id}"
 
     headers = {
-        "x-api-key": API_KEY,
-        "user-agent": str(CONTACT),
+        "x-api-key": config.API_KEY,
+        "user-agent": str(config.CONTACT),
     }
 
     print(f"{ts()} [Website] Deleting group {vrc_group_id}...")
